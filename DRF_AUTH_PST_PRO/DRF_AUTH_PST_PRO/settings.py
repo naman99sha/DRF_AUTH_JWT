31a57950e6d3e73a76b3ebe59d9e5cf8cd4cddf9
Naman Sharma <naman99sha@gmail.com>
2021-11-17 13:34:13 +0530

    Updated env related change for key

--- DRF_AUTH_PST_PRO/DRF_AUTH_PST_PRO/settings.py
+++ DRF_AUTH_PST_PRO/DRF_AUTH_PST_PRO/settings.py
@@ -13,12 +13,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 import dj_database_url
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
