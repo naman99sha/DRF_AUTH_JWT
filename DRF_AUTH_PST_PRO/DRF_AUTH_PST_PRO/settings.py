cdf64e6d933f8aae10dc08b8cca9bc4ffb3e508c
Naman Sharma <naman99sha@gmail.com>
2021-11-17 13:36:32 +0530

    Updated env related change for key

--- DRF_AUTH_PST_PRO/DRF_AUTH_PST_PRO/settings.py
+++ DRF_AUTH_PST_PRO/DRF_AUTH_PST_PRO/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
+env = environ.Env(SECRET_KEY=str,)
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
